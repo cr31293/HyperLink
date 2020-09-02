1f42fd8d0871e83ea38ac8a3a5a6a4971e808e84
Miller Rich <william.miller.rich@gmail.com>
2020-09-02 12:42:49 -0500

    create 4 new objects

---
+++ script.js
@@ -0,0 +1,40 @@
+var uTilt = new Object();
+    uTilt.row0 = ["Hitbox Id", "Damage", "Angle", "KBG", "BKB", "SKB", "Hit Lag", "Shield Stun", "Shield Adv", "Size"];
+    uTilt.row1 = ["0", "9%", "95°", "122", "30", "0", "6", "6", "-14/-9", "3.6"];
+    uTilt.row2 = ["1", "9%", "85°", "130", "30", "0", "6", "6", "-14/-9", "4.2"];
+    uTilt.row3 = ["2", "9%", "85°", "124", "30", "0", "6", "6", "-14/-9", "2.8"];
+    uTilt.row4 = ["3", "9%", "85°", "123", "30", "0", "6", "6", "-14/-9", "2.8"];
+
+var uSmash = new Object();
+    uSmash.row = ["First Hit"];
+    uSmash.row = ["Hitbox Id", "Damage", "Angle", "KBG", "BKB", "SKB", "Hit Lag", "Shield Stun", "Shield Adv", "Size"];
+    uSmash.row2 = ["0", "4", "98°", "100", "40", "26", "4", "3", "-", "3.2"];
+    uSmash.row3 = ["1", "4", "98°", "100", "40", "30", "4", "3", "-", "3.2"];
+    uSmash.row4 = ["2", "4", "98°", "100", "40", "33", "4", "3", "-", "3.2"];
+    uSmash.row5 = ["3", "4", "98°", "100", "40", "33", "4", "3", "-", "2"];
+    uSmash.row6 = ["Second Hit"];
+    uSmash.row7 = ["Hitbox Id", "Damage", "Angle", "KBG", "BKB", "SKB", "Hit Lag", "Shield Stun", "Shield Adv", "Size"];
+    uSmash.row8 = ["0", "2", "90°", "100", "0", "30", "3", "2", "-", "3.2"];
+    uSmash.row9 = ["1", "2", "90°", "100", "0", "33", "3", "2", "-", "3.2"];
+    uSmash.row10 = ["2", "2", "90°", "100", "0", "37", "3", "2", "-", "3.2"];
+    uSmash.row11 = ["3", "2", "90°", "100", "0", "40", "3", "2", "-", "2"];
+    uSmash.row12 = ["Third Hit"];
+    uSmash.row13 = ["Hitbox Id", "Damage", "Angle", "KBG", "BKB", "SKB", "Hit Lag", "Shield Stun", "Shield Adv", "Size"];
+    uSmash.row14 = ["0", "10%", "90°", "80", "90", "0", "6", "6", "-4/-2", "3.2"];
+    uSmash.row15 = ["1", "10%", "90°", "80", "90", "0", "6", "6", "-4/-2", "3.6"];
+    uSmash.row16 = ["2", "9%", "90°", "80", "90", "0", "6", "6", "-4/-2", "3.2"];
+    uSmash.row17 = ["3", "9%", "90°", "80", "90", "0", "6", "6", "-4/-2", "2"];
+
+var rapidJab = new Object();
+    rapidJab.row0 = ["Hitbox Id", "Damage", "Angle", "KBG", "BKB", "SKB", "Hit Lag", "Shield Stun", "Shield Adv", "Size"];
+    rapidJab.row1 = ["0", "1%", "0°", "40", "15", "0", "3", "2", "-10", "4.4"];
+    rapidJab.row2 = ["1", "1%", "0°", "40", "15", "0", "3", "2", "-10", "4.4"];
+    rapidJab.row3 = ["2", "1%", "0°", "40", "15", "0", "3", "2", "-10", "2.8"];
+    rapidJab.row4 = ["3", "1%", "0°", "40", "15", "0", "3", "2", "-10", "2"];
+
+var jab2 = new Object();
+    jab2.row0 = ["Hitbox Id", "Damage", "Angle", "KBG", "BKB", "SKB", "Hit Lag", "Shield Stun", "Shield Adv", "Size"];
+    jab2.row1 = ["0", "3%", "361°", "60", "10", "0", "4", "3", "-7", "4.4"];
+    jab2.row2 = ["0", "3%", "361°", "60", "10", "0", "4", "3", "-7", "4"];
+    jab2.row3 = ["0", "3%", "361°", "60", "10", "0", "4", "3", "-7", "2.4"];
+    jab2.row4 = ["0", "3%", "361°", "60", "10", "0", "4", "3", "-7", "2.4"];
